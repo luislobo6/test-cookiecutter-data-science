cb62913403786b206413b1a181c02d30b798a446
Luis Macias <el.lobo.i@gmail.com>
2022-03-12 13:33:30 -0600

    primer commit con la estructura basica del proyecto que nos genera cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this is just a test for cookiecutter repo and project',
+    author='Luis Macias',
+    license='',
+)
